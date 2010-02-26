e602c8edb55526b56f9475b94a19ab3d410589f0
Donal <Donal@192.168.2.2>
2010-02-26 13:01:39 -0800

    Updated

--- src/com/rapidftr/services/RecordService.java
+++ src/com/rapidftr/services/RecordService.java
@@ -4,11 +4,9 @@ import com.rapidftr.model.ChildRecord;
 import com.rapidftr.model.ChildRecordItem;
 
 public interface RecordService {
-	String getRecordId() throws ServiceException;
-
 	ChildRecordItem[] getMatches(String searchCriteria) throws ServiceException;
 	
-	ChildRecord getRecord(String recordId) throws ServiceException;
+	ChildRecord getRecord(ChildRecordItem item) throws ServiceException;
 	
 	void save(ChildRecord record) throws ServiceException;
 }
