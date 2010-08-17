e86288b07478d214f2fc179f726cb91afc2725c0
unknown <vino@.(none)>
2010-08-17 23:47:42 +0530

    Added tests around httpservice <Vinoth>

--- RapidFTR-Blackberry-Tests/src/com/rapidftr/net/HttpServiceTest.java
+++ RapidFTR-Blackberry-Tests/src/com/rapidftr/net/HttpServiceTest.java
@@ -1,64 +1,70 @@
 package com.rapidftr.net;
 
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.rapidftr.utilities.SettingsStore;
 import com.sun.me.web.request.Arg;
+import com.sun.me.web.request.PostData;
 import com.sun.me.web.request.RequestListener;
 import static org.mockito.Mockito.when;
 
 public class HttpServiceTest {
 
 	private HttpService httpService;
+	@Mock
 	private HttpServer httpServer;
+	@Mock
 	private RequestListener listener;
 	private String url;
+	@Mock
 	private Object context;
+	@Mock
 	SettingsStore settingsStore;
 	private Arg[] httpArgs;
+	private Arg[] httpArgsWithAuthToken;
 
 	@Before
 	public void setUp() {
-		httpServer = mock(HttpServer.class);
-		settingsStore = mock(SettingsStore.class);
+		MockitoAnnotations.initMocks(this);
 		httpService = new HttpService(httpServer, settingsStore);
-		listener = mock(RequestListener.class);
 		url = "url";
-		context = mock(Object.class);
+		String token = "token";
+		httpArgs = new Arg[0];
+		httpArgsWithAuthToken = new Arg[] { new Arg(Arg.AUTHORIZATION,
+				"RFTR_Token " + token) };
+		when(settingsStore.getAuthorizationToken()).thenReturn(token);
 		when(httpServer.buildFullyQualifiedUrl(url)).thenReturn(url);
 	}
 
 	@Test
 	public void shouldSendPostResquestToHttpServer() {
-	
-
 		Arg[] postParams = new Arg[0];
-		httpService.post(url, postParams , listener, context);
-		
-		verify(httpServer).postToServer(url,
-				postParams, httpArgs, listener, context, null);
+		Arg[] postArgs = new Arg[0];
+		PostData postData = mock(PostData.class);
+		httpService
+				.post(url, postParams, postArgs, listener, postData, context);
+		verify(httpServer).postToServer(url, postParams, httpArgsWithAuthToken,
+				listener, postData, context);
 	}
 
 	@Test
 	public void shouldSendGetRequestToHttpServer() {
 		Arg[] inputParams = new Arg[0];
-		httpService.get(url, inputParams, listener);
-		
-		
-		verify(httpServer).getFromServer(url,
-				inputParams,httpArgs, listener);
+		httpService.get(url, inputParams, httpArgs, listener);
+		verify(httpServer).getFromServer(url, inputParams,
+				httpArgsWithAuthToken, listener);
 	}
 
 	@Test
 	public void shouldCancelCurrentRequest() {
 		httpService.cancelRequest();
 		verify(httpServer).cancelRequest();
-
 	}
 
 }
