2c9552d5b26d23fea423e545f8ae6cb14447451c
unknown <suchitp@.corporate.thoughtworks.com>
2010-08-19 16:20:38 +0530

    SyncAll

---
+++ RapidFTR-Blackberry/src/com/rapidftr/controllers/SyncAllController.java
@@ -0,0 +1,19 @@
+package com.rapidftr.controllers;
+
+import com.rapidftr.screens.CustomScreen;
+import com.rapidftr.screens.UiStack;
+
+public class SyncAllController extends Controller{
+
+	public SyncAllController(CustomScreen screen, UiStack uiStack) {
+		super(screen, uiStack);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void syncAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
