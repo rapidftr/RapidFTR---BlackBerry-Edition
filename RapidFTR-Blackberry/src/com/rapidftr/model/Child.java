5970c1b93a9da45fdaa13f52ab3f6ed98546702c
suchitpuri <suchitp@thoughtworks.com>
2010-08-20 12:11:48 +0530

    Override equals method of Child based on id

--- RapidFTR-Blackberry/src/com/rapidftr/model/Child.java
+++ RapidFTR-Blackberry/src/com/rapidftr/model/Child.java
@@ -86,4 +86,34 @@ public class Child implements Persistable {
 		return data;
 	}
 
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((data == null) ? 0 : data.get("_id").hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Child other = (Child) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else {
+			if (data.get("_id") == null) {
+				return false;
+			}
+			if (!data.get("_id").equals((other.data.get("_id"))))
+				return false;
+		}
+
+		return true;
+	}
+
 }
