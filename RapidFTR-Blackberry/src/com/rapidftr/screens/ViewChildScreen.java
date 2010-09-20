a01bfe2dd266dbe2fdfedae830b8813bb9550b91
suchitpuri <suchitp@thoughtworks.com>
2010-09-20 10:32:09 +0530

    fixed ordering of view child detail to same as new/edit child .... need to look in code again to move logic in appropriate layer (cherry picking from commit aaac001a7ec59fcfa5118b23a3f3a0d0f6942c37) Merge branch 'master' of github.com:sugupta4/RapidFTR---BlackBerry-Edition (cherry picking from commit c76f4b19d418beba5b71015bf5ee5598ebd174e5)

--- RapidFTR-Blackberry/src/com/rapidftr/screens/ViewChildScreen.java
+++ RapidFTR-Blackberry/src/com/rapidftr/screens/ViewChildScreen.java
@@ -49,17 +49,19 @@ public class ViewChildScreen extends CustomScreen {
 
 	private void renderChildFields(Child child) {
 		//updateChildFieldsWithLatestForms(child);
+		int index = 0;
+		
 		Hashtable data = child.getKeyMap();
 		HorizontalFieldManager hmanager = new HorizontalFieldManager(Manager.HORIZONTAL_SCROLLBAR);
 		renderBitmap(data, hmanager);
 
 		hmanager.add(new BoldRichTextField("   " + data.get(new String("name"))));
 		add(hmanager);
-		RichTextField richField[] = new RichTextField[data.size()];
-		int index = 0;
-
+		
 		Vector forms = new FormStore().getForms();
-
+		
+		RichTextField richField[] = new RichTextField[claculateTotalElementsInForms(forms)];
+	
 		for (Enumeration list = forms.elements(); list.hasMoreElements();) {
 			Form form = (Form) list.nextElement();
 			for (Enumeration fields = form.getFieldList().elements(); fields.hasMoreElements();) {
@@ -156,5 +158,17 @@ public class ViewChildScreen extends CustomScreen {
 		} catch (IllegalArgumentException ex) {
 			return null;
 		}
+	}
+	
+	private int claculateTotalElementsInForms(Vector forms)
+	{
+		int totalCount=0;
+		for(int i=0 ;i< forms.size();i++)
+		{
+			Form form = (Form)forms.elementAt(i);
+			totalCount= totalCount+ form.getFieldList().size();
+		}
+		
+		return totalCount;
 	}
 }
