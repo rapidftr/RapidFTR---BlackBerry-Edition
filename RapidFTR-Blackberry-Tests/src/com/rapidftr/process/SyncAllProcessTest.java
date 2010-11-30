39cc016c3fa6ddf4f23d92813f111500b156c538
shaobo <wushaobo.china@gmail.com>
2010-11-30 10:16:41 +0800

    shaobo & xia - fixed the build

--- RapidFTR-Blackberry-Tests/src/com/rapidftr/process/SyncAllProcessTest.java
+++ RapidFTR-Blackberry-Tests/src/com/rapidftr/process/SyncAllProcessTest.java
@@ -2,12 +2,12 @@ package com.rapidftr.process;
 
 import static org.mockito.Mockito.verify;
 
+import com.rapidftr.services.FormService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.rapidftr.model.Child;
 import com.rapidftr.services.ChildSyncService;
 
 public class SyncAllProcessTest {
@@ -15,26 +15,27 @@ public class SyncAllProcessTest {
 	@Mock
 	private SyncAllProcess syncAllProcess;
 	@Mock
-	private ChildSyncService service;
-	
+	private ChildSyncService childService;
+    @Mock
+    private FormService formService;
 
-	@Before
+
+    @Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
-		syncAllProcess = new SyncAllProcess(service);
-
+		syncAllProcess = new SyncAllProcess(childService, formService);
 	}
 
 	@Test
 	public void startProcessShouldSyncAll() {
 		syncAllProcess.startProcess();
-		verify(service).syncAllChildRecords();
+		verify(childService).syncAllChildRecords();
 	}
 
 	@Test
 	public void stopProcessShouldStopSyncAll() {
 		syncAllProcess.stopProcess();
-		verify(service).cancelRequest();
+		verify(childService).cancelRequest();
 	}
 
 }
