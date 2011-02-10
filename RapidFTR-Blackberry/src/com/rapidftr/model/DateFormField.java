9c30d381147b69b9557ec4d9814fb9b57e65ce29
rgan <rajaram.ganeshan@gmail.com>
2011-02-10 18:15:17 -0500

    Fixing date field format

--- RapidFTR-Blackberry/src/com/rapidftr/model/DateFormField.java
+++ RapidFTR-Blackberry/src/com/rapidftr/model/DateFormField.java
@@ -1,11 +1,15 @@
 package com.rapidftr.model;
 
+import net.rim.device.api.i18n.DateFormat;
+import net.rim.device.api.i18n.SimpleDateFormat;
 import net.rim.device.api.io.http.HttpDateParser;
 import net.rim.device.api.ui.Manager;
 import net.rim.device.api.ui.component.DateField;
 import net.rim.device.api.ui.container.VerticalFieldManager;
 
 import com.rapidftr.screens.ManageChildScreen;
+
+import java.util.Date;
 
 public class DateFormField extends FormField
 {
@@ -24,17 +28,17 @@ public class DateFormField extends FormField
 	}
 
 	public String getValue() {
-        String date = dateField.toString();
-        if ("- --- ----".equals(date)){
+        if (dateField.getDate() == Long.MIN_VALUE) {
             return "";
         }
-        return date;
+        return new SimpleDateFormat("d MMM yyyy").format(new Date(dateField.getDate()));
 	}
 
-	public void initializeLayout(ManageChildScreen newChildScreen) {
+    public void initializeLayout(ManageChildScreen newChildScreen) {
 		
 		manager=new VerticalFieldManager();
-		dateField=new DateField(label, System.currentTimeMillis(), DateField.DATE);
+        DateFormat dateFormat = new SimpleDateFormat("d MMM yyyy");
+		dateField=new DateField(label, Long.MIN_VALUE, dateFormat, DateField.DATE);
         dateField.setDate(null);
 	    manager.add(dateField);
 	}
