c1a1af6ec737720b208f98561a6ac4531145c57b
Roger Stevens <rms.ftr@gmail.com>
2011-04-13 09:35:27 +0100

    I created a 542B branch in order to add a mechanism to change field labels with out having to recompile. Using Blackberry's internationalization feature this is possible. In the Blackberry simulator this works fine. However there are a couple of issues which I dont know how to handle. First the inclusion of I18N breaks the test suite. As the Blackberry I18N uses native methods there is no windows/unix dll for the java code to link with hence the test code throws UnsatifiedLinkError when the test FormFieldFactoryTests are run. Secondly I have not tested a language specific resource bundle only the basic default bundle file which is named ModelLabel.rcc and ModelLabel.rrh. These reside in a new directory 'res' at the same level as 'src' directory following the same package structure as the code that uses it. This because of the way I18N works on the Blackberry. The reason these tests have not been carried out is that I dont know where the json strings are comming from that are used in the simulator and whether I have privilege to change them to carry out the test.
    	modified:   RapidFTR-Blackberry/.classpath
    	modified:   RapidFTR-Blackberry/src/com/rapidftr/model/NumericTextboxFormField.java
    	modified:   build.properties
    	modified:   build.xml
    	RapidFTR-Blackberry/.locale_interfaces/
    	RapidFTR-Blackberry/res/
    	RapidFTR-Blackberry/src/com/rapidftr/model/ModelLabelText.java

--- RapidFTR-Blackberry/src/com/rapidftr/model/NumericTextboxFormField.java
+++ RapidFTR-Blackberry/src/com/rapidftr/model/NumericTextboxFormField.java
@@ -1,17 +1,28 @@
 package com.rapidftr.model;
 
 import net.rim.device.api.ui.text.NumericTextFilter;
+import net.rim.device.api.i18n.ResourceBundle;
 
 import com.rapidftr.screens.ManageChildScreen;
+//import com.rapidftr.model.NumericTextboxResource;
 
-public class NumericTextboxFormField extends TextboxFormField {
 
+public class NumericTextboxFormField extends TextboxFormField  {
+	
+    public static String AGELABELSTRING = ModelLabelText.getAgeLabelString();
+	
+	
 	public NumericTextboxFormField(String name, String displayName, String type) {
 		super(name, displayName, type);
 	}
 
 	public void initializeLayout(ManageChildScreen newChildScreen) {
 		super.initializeLayout(newChildScreen);
+
 		field.setFilter(new NumericTextFilter());
+		
+		if (AGELABELSTRING.equals(name)){
+			field.setMaxSize(2);
+		}
 	}
 }
