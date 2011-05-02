07e9d3095dd5fe50fa2c062af99508d81aaab613
sugiyarto_s@yahoo.com <user@Sugis-Mac.local>
2011-05-02 16:32:27 +1000

    ST: Moved HomeController creation from Main to Dispatcher, add Dispatcher as constructor parameter in HomeController.

--- RapidFTR-Blackberry/src/com/rapidftr/controllers/HomeController.java
+++ RapidFTR-Blackberry/src/com/rapidftr/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.rapidftr.controllers;
 
 import com.rapidftr.controllers.internal.Controller;
+import com.rapidftr.controllers.internal.Dispatcher;
 import com.rapidftr.screens.internal.CustomScreen;
 import com.rapidftr.screens.internal.UiStack;
 import com.rapidftr.utilities.Settings;
@@ -9,38 +10,39 @@ import net.rim.blackberry.api.browser.Browser;
 public class HomeController extends Controller {
     private Settings settings;
 
-    public HomeController(CustomScreen screen, UiStack uiStack, Settings settings) {
-		super(screen, uiStack);
+    public HomeController(CustomScreen homeSreen, UiStack uiStack, Settings settings, Dispatcher dispatcher) {
+        super(homeSreen, uiStack);
         this.settings = settings;
+        this.dispatcher = dispatcher;
     }
 
-	public void viewChildren() {
-		dispatcher.viewChildren();
-	}
+    public void viewChildren() {
+        dispatcher.viewChildren();
+    }
 
-	public void newChild() {
-		dispatcher.newChild();
-	}
+    public void newChild() {
+        dispatcher.newChild();
+    }
 
-	public void synchronize() {
-		dispatcher.synchronize();
-	}
+    public void synchronize() {
+        dispatcher.synchronize();
+    }
 
-	public void showSearch() {
-		dispatcher.searchChild();
-	}
+    public void showSearch() {
+        dispatcher.searchChild();
+    }
 
-	public void cleanAll() {
-		dispatcher.resetDevice();
-	}
+    public void cleanAll() {
+        dispatcher.resetDevice();
+    }
 
-	public void logIn() {
-		dispatcher.login(null);
-	}
+    public void logIn() {
+        dispatcher.login(null);
+    }
 
-	public void showcontact() {
-		dispatcher.showcontact();
-	}
+    public void showcontact() {
+        dispatcher.showcontact();
+    }
 
     public void updateApplication() {
         Browser.getDefaultSession().displayPage(settings.getApplicationUpdateUrl());
