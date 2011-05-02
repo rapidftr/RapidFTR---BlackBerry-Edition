91a1050bcef95aecb0678635afc49889b1930e8a
sugiyarto_s@yahoo.com <user@Sugis-Mac.local>
2011-05-02 13:35:08 +1000

    ST: Added tests for ContactInformationController and ContactInformationSyncService.

---
+++ RapidFTR-Blackberry-Tests/src/com/rapidftr/controllers/internal/ContactInformationControllerTest.java
@@ -0,0 +1,33 @@
+package com.rapidftr.controllers.internal;
+
+import com.rapidftr.controllers.ContactInformationController;
+import com.rapidftr.screens.ContactInformationScreen;
+import com.rapidftr.screens.internal.UiStack;
+import com.rapidftr.services.ContactInformationSyncService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ContactInformationControllerTest {
+
+    private ContactInformationScreen screen;
+    private UiStack uiStack;
+    private ContactInformationSyncService service;
+    private ContactInformationController controller;
+
+    @Before
+    public void setup() {
+        screen = mock(ContactInformationScreen.class);
+        uiStack = mock(UiStack.class);
+        service = mock(ContactInformationSyncService.class);
+        controller = new ContactInformationController(screen, uiStack, service);
+    }
+
+    @Test
+    public void shouldDownloadContactInformation() {
+        controller.fetchContactInformation();
+        verify(service).downloadContactInformation();
+    }
+}
