46a585b09abc24e13110a1221741294134a6dfba
unknown <balajin@.corporate.thoughtworks.com>
2011-05-03 10:48:56 +0530

    [BalajiN, ZabilC] Adding more tests tor TabsField

--- RapidFTR-Blackberry-Tests/src/com/rapidftr/model/TabsFieldTest.java
+++ RapidFTR-Blackberry-Tests/src/com/rapidftr/model/TabsFieldTest.java
@@ -1,9 +1,11 @@
 package com.rapidftr.model;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import net.rim.device.api.ui.container.HorizontalFieldManager;
 import net.rim.device.api.ui.container.VerticalFieldManager;
 
@@ -14,8 +16,8 @@ public class TabsFieldTest {
 	@Test
 	public void openDefaultTabOnDraw() {
 		TabsField tabsField = createTabsField();
-		Tab tab1 = createTab();
-		Tab tab2 = createTab();
+		Tab tab1 = createTab("tab1");
+		Tab tab2 = createTab("tab2");
 
 		tabsField.addTab(tab1);
 		tabsField.addTab(tab2);
@@ -25,22 +27,11 @@ public class TabsFieldTest {
 		verify(tab2, never()).open();
 	}
 
-	private Tab createTab() {
-		return spy(new Tab(mock(Form.class), null,
-				mock(TabLabelField.class), mock(TabBodyField.class)));
-	}
-
-	private TabsField createTabsField() {
-		return new TabsField(mock(VerticalFieldManager.class),
-				mock(HorizontalFieldManager.class),
-				mock(VerticalFieldManager.class));
-	}
-
 	@Test
 	public void toggleSelectedTab() {
 		TabsField tabsField = createTabsField();
-		Tab tab1 = createTab();
-		Tab tab2 = createTab();
+		Tab tab1 = createTab("tab1");
+		Tab tab2 = createTab("tab2");
 
 		tabsField.addTab(tab1);
 		tabsField.addTab(tab2);
@@ -50,5 +41,36 @@ public class TabsFieldTest {
 
 		verify(tab1).close();
 		verify(tab2).open();
+	}
+
+	@Test
+	public void getSelectedTabName() throws Exception {
+		TabsField tabsField = createTabsField();
+		Tab tab1 = createTab("tab1");
+		Tab tab2 = createTab("tab2");
+
+		tabsField.addTab(tab1);
+		tabsField.addTab(tab2);
+		tabsField.draw();
+		
+		assertEquals("tab1", tabsField.getSelectedTab());
+		tab2.focusChanged(mock(TabLabelField.class), 1);
+		assertEquals("tab2", tabsField.getSelectedTab());
+
+
+	}
+
+	private Tab createTab(String label) {
+		TabLabelField mockLabelField = mock(TabLabelField.class);
+		when(mockLabelField.getText()).thenReturn(label);
+		return spy(new Tab(mock(Form.class), null, mockLabelField,
+				mock(TabBodyField.class)));
+		
+	}
+
+	private TabsField createTabsField() {
+		return new TabsField(mock(VerticalFieldManager.class),
+				mock(HorizontalFieldManager.class),
+				mock(VerticalFieldManager.class));
 	}
 }
